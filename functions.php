54cc1ac112da928ed0fe5b698b9bdc2d12d8e2de
Cody <cody@codywhitby.net>
2016-09-07 13:22:32 -0400

    Built from commit 7cb3c6f on branch heads/upstream/master

--- functions.php
+++ functions.php
@@ -86,7 +86,7 @@ add_action( 'customize_register', 'bones_theme_customizer' );
  * EXTERNAL FONTS
  */
 function bones_fonts() {
-  wp_enqueue_style( 'googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' );
+  wp_enqueue_style( 'googleFonts', 'https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' );
 }
 
 add_action( 'wp_enqueue_scripts', 'bones_fonts' );
